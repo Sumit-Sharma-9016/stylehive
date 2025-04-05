7a558d2f589dafcfcc3e1af05519e539b28dc72d
Sumit Sharma <sumit901693@gmail.com>
2025-04-05 13:41:44 +0530

    style and url

--- Frontend/src/components/Collection/Item.jsx
+++ Frontend/src/components/Collection/Item.jsx
@@ -10,11 +10,11 @@ function Item({product}) {
     });
     
     return (
-        <Link to={`/item/${_id}`} className='cursor-pointer justify-self-center bg-gray-200 shrink-0 flex flex-col items-center md:h-50 md:w-35 xl:h-65 xl:w-50 h-40 w-25 overflow-hidden'>
-            <div className='img-container md:h-35 md:w-35 xl:h-50 xl:w-50 h-30 w-25 overflow-hidden bg-gray-100'>
+        <Link to={`/item/${_id}`} className='cursor-pointer justify-self-center bg-gray-200 shrink-0 flex flex-col items-center md:h-50 md:w-35 xl:h-65 xl:w-50 h-40 w-28 overflow-hidden'>
+            <div className='img-container md:h-35 md:w-35 xl:h-50 xl:w-50 h-30 w-28 overflow-hidden bg-gray-100'>
                 <img src={images[0].url} alt="" />
             </div>
-            <div className='product-details flex flex-col justify-start h-fit w-25 md:h-15 md:w-35 xl:h-15 xl:w-50 lg:p-2 p-1 text-nowrap overflow-hidden'>
+            <div className='product-details flex flex-col justify-start h-fit w-28 md:h-15 md:w-35 xl:h-15 xl:w-50 lg:p-2 p-1 text-nowrap overflow-hidden'>
                 <p className='md:text-[0.8rem] lg:text-[0.9rem] text-start lg:text-nowrap text-[0.5rem] overflow-hidden text-gray-700'>{title}</p>
                 <div className='flex gap-2 items-center md:text-[.8rem] text-[0.6rem] text-gray-700'>
                     <p className='font-semibold text-gray-900'>&#8377;{price}</p>
